c6673d1b71870afc36846fd6cfe2ae35430b0a88
Zartab Nakhwa <zartab@codewithz.com>
2022-03-03 17:59:07 +0530

    Dashboard: Left Panel Model Data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  designation: string = '';
+  username: string = '';
+  noOfTeamMembers: number = 0;
+  totalCostOfAllProjects: number = 0;
+  pendingTasks: number = 0;
+  upcomingProjects: number = 0;
+  projectCost: number = 0;
+
+  currentExpenditure: number = 0;
+  availableFunds: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.designation = 'Team Leader';
+    this.username = 'Tom Smith';
+    this.noOfTeamMembers = 65;
+    this.totalCostOfAllProjects = 255;
+    this.pendingTasks = 15;
+    this.upcomingProjects = 3;
+    this.projectCost = 2334609;
+    this.currentExpenditure = 87998;
+    this.availableFunds = 43534;
+
   }
 
 }
