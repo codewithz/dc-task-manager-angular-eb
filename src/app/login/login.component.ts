d28092675a1782db6c3b66d3e63b5c7091145b6e
Zartab Nakhwa <zartab@codewithz.com>
2022-03-08 16:28:40 +0530

    Login Page: Basic Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Login } from './login';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +13,27 @@ export class LoginComponent implements OnInit {
   login: Login = new Login();
   errorMessage: string = '';
 
-  constructor() { }
+  constructor(private service: LoginService, private router: Router) {
+    this.login.UserName = 'admin';
+    this.login.Password = 'Admin123#'
+
+  }
 
   ngOnInit(): void {
+  }
+
+  onLogin() {
+    this.service.loginUser(this.login)
+      .subscribe(
+        (response) => {
+          console.log(response)
+          this.router.navigate(["/dashboard"])
+        },
+        (error) => {
+          console.log(error)
+          this.errorMessage = 'Invalid Username or password'
+        }
+      )
   }
 
 }
