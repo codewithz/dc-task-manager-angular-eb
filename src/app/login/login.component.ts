5dd26801d66e8d7fdc795dc9a91e4d2b2b541c27
Zartab Nakhwa <zartab@codewithz.com>
2022-03-08 16:12:04 +0530

    Login: Model and HTML is connected

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from './login';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  login: Login = new Login();
+  errorMessage: string = '';
 
   constructor() { }
 
