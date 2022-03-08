28b81c7860ab92df1e1361487b117c0d3176d32a
Zartab Nakhwa <zartab@codewithz.com>
2022-03-08 17:14:58 +0530

    Login: Navbar Implementation

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -2,17 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Login } from './login';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  currentUser: any = null;
+
   private url: string = 'http://localhost:9090/authenticate'
 
   constructor(private http: HttpClient) { }
 
   loginUser(login: Login): Observable<any> {
-    return this.http.post<any>(this.url, login);
+    return this.http.post<any>(this.url, login)
+      .pipe(
+        map((user: any) => {
+          this.currentUser = user.userName
+        })
+      )
+  }
+
+  logoutUser() {
+    this.currentUser = null;
+  }
+
+  isUserLoggedIn() {
+    return this.currentUser != null ? true : false;
   }
 }
