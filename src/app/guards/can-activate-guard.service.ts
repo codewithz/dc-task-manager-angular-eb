c7dbe54ab0525e26aae0fe0f8a73912ea565c15a
Zartab Nakhwa <zartab@codewithz.com>
2022-03-09 15:50:13 +0530

    Role Based AUthorization

--- src/app/guards/can-activate-guard.service.ts
+++ src/app/guards/can-activate-guard.service.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from '../login/login.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
 export class CanActivateGuardService implements CanActivate {
 
-  constructor(private loginService: LoginService, private router: Router) { }
+  constructor(private loginService: LoginService, private router: Router, private jwtHelperService: JwtHelperService) { }
 
-  canActivate(): boolean {
+  canActivate(route: ActivatedRouteSnapshot): boolean {
 
-    if (this.loginService.isAuthenticated()) {
+    const token = sessionStorage.getItem('currentUser') ?
+      JSON.parse(sessionStorage.getItem('currentUser') as string).token : null;
+
+    const decodedToken = this.jwtHelperService.decodeToken(token);
+
+    const role = decodedToken.role;
+
+
+    if (this.loginService.isAuthenticated() && route.data.roles.includes(role)) {
       return true;
     }
     else {
