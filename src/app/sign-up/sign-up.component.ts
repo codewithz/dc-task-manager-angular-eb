bea3b59238f1a558aaa1f6226b4b8992b249ff98
Zartab Nakhwa <zartab@codewithz.com>
2022-03-11 17:50:13 +0530

    RF:patchValue

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -49,17 +49,23 @@ export class SignUpComponent implements OnInit {
 
   onCreateAccountClicked() {
     // 1. setValue
-    this.signUpForm.setValue({
+    // this.signUpForm.setValue({
+    //   firstName: 'Zartab',
+    //   lastName: 'Nakhwa',
+    //   email: 'zartab@codewithz.com',
+    //   mobile: '7715036251',
+    //   dateOfBirth: '1990-12-13',
+    //   gender: 'male',
+    //   countryID: 300
+    // })
+
+    // 2.patchValue
+    this.signUpForm.patchValue({
       firstName: 'Zartab',
       lastName: 'Nakhwa',
       email: 'zartab@codewithz.com',
-      mobile: '7715036251',
-      dateOfBirth: '1990-12-13',
-      gender: 'male',
-      countryID: 300
-    })
 
-    // 2.patchValue
+    })
     // 3.reset
   }
 
