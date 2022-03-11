86ba524c5a5aea6ddc6cf290e2783ad179cd98e0
Zartab Nakhwa <zartab@codewithz.com>
2022-03-11 16:20:38 +0530

    RF: Import ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JwtModule } from '@auth0/angular-jwt';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -42,6 +42,7 @@ import { ProjectIdUniqueValidatorDirective } from './directives/project-id-uniqu
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     JwtModule.forRoot(
       {
         config: {
