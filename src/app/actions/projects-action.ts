29b641302db3bcf2f2e1ca4fbd193c9e85503bd0
Zartab Nakhwa <zartab@codewithz.com>
2022-03-15 18:10:22 +0530

    Redux: Project Add- Action Added

--- src/app/actions/projects-action.ts
+++ src/app/actions/projects-action.ts
@@ -3,6 +3,7 @@ import { Project } from './../projects/project';
 
 export const PROJECTS_LIST_REQUESTED = 'PROJECTS_LIST_REQUESTED';
 export const PROJECTS_LIST_SUCCESS = 'PROJECTS_LIST_SUCCESS';
+export const PROJECT_ADDED = 'PROJECT_ADDED';
 
 
 export class ProjectsListRequestedAction {
@@ -12,4 +13,9 @@ export class ProjectsListRequestedAction {
 export class ProjectsListSuccessAction {
     readonly type = PROJECTS_LIST_SUCCESS;
     constructor(public payload?: { data: Project[] }) { }
+}
+
+export class ProjectAddedAction {
+    readonly type = PROJECT_ADDED;
+    constructor(public payload?: { data: Project }) { }
 }
