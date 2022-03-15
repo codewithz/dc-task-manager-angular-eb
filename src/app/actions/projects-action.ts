c81040bd3b17db2fba74742a18c71e7e1dd0fd2f
Zartab Nakhwa <zartab@codewithz.com>
2022-03-15 16:04:50 +0530

    Redux: Defined Action | ActionType | Action Class

---
+++ src/app/actions/projects-action.ts
@@ -0,0 +1,15 @@
+
+import { Project } from './../projects/project';
+
+export const PROJECTS_LIST_REQUESTED = 'PROJECTS_LIST_REQUESTED';
+export const PROJECTS_LIST_SUCCESS = 'PROJECTS_LIST_SUCCESS';
+
+
+export class ProjectsListRequestedAction {
+    readonly type = PROJECTS_LIST_REQUESTED;
+}
+
+export class ProjectsListSuccessAction {
+    readonly type = PROJECTS_LIST_SUCCESS;
+    constructor(public payload?: { data: Project[] }) { }
+}
