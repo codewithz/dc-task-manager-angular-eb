617cebabbb54b26b4631cc9ba47810aa75061eae
Zartab Nakhwa <zartab@codewithz.com>
2022-03-15 16:20:57 +0530

    Redux: Created projectReducer

--- src/app/reducers/project-reducer.ts
+++ src/app/reducers/project-reducer.ts
@@ -1,7 +1,31 @@
+import { Action } from '../actions';
 import { Project } from './../projects/project';
+import * as projectActions from '../actions/projects-action';
 
 export interface ProjectReducerState {
     loading: boolean,
     loaded: boolean,
     projects: Project[]
+}
+
+const initialState: ProjectReducerState = {
+    loading: false,
+    loaded: false,
+    projects: []
+}
+
+export function userReducer(state = initialState, action: Action) {
+    switch (action.type) {
+        case projectActions.PROJECTS_LIST_REQUESTED: {
+            return { ...state, loading: true };
+        };
+        case projectActions.PROJECTS_LIST_SUCCESS: {
+            const updatedProjects = state.projects.concat(action.payload.data);
+            return { ...state, loading: false, loaded: true, projects: updatedProjects }
+        };
+        default: {
+            return state;
+        }
+
+    }
 }
